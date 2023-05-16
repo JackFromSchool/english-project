97d75e0d58645d340f484022e1c5a2c8054cf492
JackFromSchool <jack.p.saenz@gmail.com>
2023-05-16 01:24:19 -0500

    Init

---
+++ tailwind.config.cjs
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+      colors: {
+         hg: '#386641',
+         as: '#90ba78',
+         yg: '#a7c957',
+         pm: '#f2e8cf',
+         pmd: '#ede0bf',
+         bs: '#bc4749',
+         bsl: '#c9696b',
+      },
+      patterns: {
+        opacities: {
+            100: "1",
+            80: ".80",
+            60: ".60",
+            40: ".40",
+            20: ".20",
+            10: ".10",
+            5: ".05",
+         },
+         sizes: {
+            1: "0.25rem",
+            2: "0.5rem",
+            4: "1rem",
+            6: "1.5rem",
+            8: "2rem",
+            16: "4rem",
+            20: "5rem",
+            24: "6rem",
+            32: "8rem",
+         }
+      },
+	},
+	plugins: [
+      require('tailwindcss-bg-patterns')
+   ],
+}
